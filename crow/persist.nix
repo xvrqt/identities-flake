49efafed4a7a8427751eace9bf42a8cb0d6cb377
crow <git@xvrqt.com>
2025-08-03 15:07:46 -0700

    Update crow/persist.nix

--- crow/persist.nix
+++ crow/persist.nix
@@ -2,7 +2,7 @@
 let
   # If this identity is enabled
   identityEnabled = config.identities.${user.name}.enable;
-  persistCheck = (config.environment.persistence.enable == true);
+  persistCheck =config.environment?persistence && config.environment.persistence?"/persist" && config.environment.persistence."/persist"?enable && (config.environment.persistence."/persist".enable == true);
   cfgCheck = persistCheck && identityEnabled;
 in
 if cfgCheck then
