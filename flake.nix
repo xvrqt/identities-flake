daa8ac413a6056db57b6b2053a85b7adf4dbbeed
crow <git@xvrqt.com>
2025-08-03 15:14:20 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -23,6 +23,11 @@
           ];
           options = {
             identities = {
+              impermanence = lib.mkOption {
+                type = lib.types.bool;
+                default = true;
+                description = "If the user should have impermanence set up for them."
+              };
               autoLogin = lib.mkOption
                 {
                   type = lib.types.enum [ "crow" ];
